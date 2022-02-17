37869bfe882d988758bf9b661ba078debea3171d
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-16 23:17:08 -0500

    add api call async fetch

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,16 @@
 import MovieList from "../MovieList/MovieList";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
 
 function Home() {
+
+useEffect(() => {
+  const movieText = "Harry";
+  const fetchMovies = async() => {
+     const response = await movieApi.get(`?apiKey=${APIKey}&s={movieText}&type=movie`);
+  }
+})
+
     return (
      <div className="banner-img">
        <MovieList />
