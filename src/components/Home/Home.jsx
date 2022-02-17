5c8d3c092d0aaaf28eb9d2acdc05081bd2244491
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-16 23:28:15 -0500

    add api call

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,14 +2,22 @@ import MovieList from "../MovieList/MovieList";
 import movieApi from "../../common/apis/movieApi";
 import { APIKey } from "../../common/apis/MovieApiKey";
 
-function Home() {
+const Home = () => {
 
 useEffect(() => {
   const movieText = "Harry";
-  const fetchMovies = async() => {
-     const response = await movieApi.get(`?apiKey=${APIKey}&s={movieText}&type=movie`);
-  }
-})
+  const fetchMovies = async () => {
+     const response = await movieApi
+     .get(`?apiKey=${APIKey}&s={movieText}&type=movie`
+     .catch((err) => {
+       console.log("Err :", err);
+     })
+
+     console.log("the response from api", response)
+  };
+
+  fetchMovies();
+}, []);
 
     return (
      <div className="banner-img">
