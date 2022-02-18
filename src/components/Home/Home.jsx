2b3863454dcfd40ba1286a1c986a6febb24fee73
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-17 21:22:03 -0500

    put use dispatch outside use effect hook

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,15 @@ import { useEffect } from 'react';
 import MovieList from "../MovieList/MovieList";
 import movieApi from "../../common/api/movieApi";
 import { APIKey } from "../../common/api/MovieApiKey";
+import { useDispatch } from "react-redux";
 import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
 
+const movieText = "Harry";
+const dispatch = useDispatch();
+
 useEffect(() => {
-  const movieText = "Harry";
-  const dispatch = useDispatch();
   const fetchMovies = async () => {
      const response = await movieApi
      .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
