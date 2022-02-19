a49d8683c8a49fc736cbc43f3afa73edb8328a54
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-19 12:40:10 -0500

    map over movies

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,23 @@
 import { useSelector } from "react-redux";
 import { getAllMovies } from "../../features/movies/movieSlice";
+import MovieCard from "../MovieCard/MovieCard";
 
 function MovieList() {
 
   const movies = useSelector(getAllMovies);
-  console.log(movies);
+  let renderMovies = "";
 
-    return (
-      <div>MovieList</div>
+  renderMovies =
+   movies.Response === "True" ? (
+    movies.Search.map((movie, index) => {
+      <MovieCard key={index} data={movie} />;
+    })
+  ) : (
+    <div className="movies-error">
+      <h3>{movies.Error}</h3>
+    </div>
     );
+    return <div>MovieList</div>;
   };
   
   export default MovieList;
