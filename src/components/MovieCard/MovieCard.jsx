64759078ddd370dceaf996e335a5443ab02f0354
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-06 17:11:00 -0500

    add link to movie card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,4 +1,5 @@
 import "./MovieCard.scss";
+import { Link } from 'react-router-dom';
 
 function MovieCard(props) {
 
@@ -6,6 +7,7 @@ function MovieCard(props) {
 
     return (
       <div className="card-item">
+      <Link to={`/movie/:${data.imdbID}`}>
        <div className="card-inner">
          <div className="card-top">
            <img src={data.Poster} alt={data.Title} />
@@ -17,6 +19,7 @@ function MovieCard(props) {
            </div>
          </div>
        </div>
+       </Link>
       </div>
     );
   };
