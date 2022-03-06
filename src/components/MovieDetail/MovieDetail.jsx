7537deb0c0b8abc18a0e77ebabdf4cd07e736fa4
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-06 17:44:37 -0500

    add movie detail

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -14,7 +14,53 @@ function MovieDetail() {
     dispatch(fetchAsyncMovieOrShowDetail(imdbID));
   }, [dispatch, imdbID]);
   return (
-    <div>MovieDetail</div>
+    <div className="movie-section">
+      <div className="section-left">
+        <div className="movie-title">{data.Title}</div>
+        <div className="movie-rating">
+          <span>
+            IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
+          </span>
+          <span>
+            IMDB Votes <i className="fa fa-thumbs-up"></i> : {data.imdbVotes}
+          </span>
+          <span>
+            Runtime <i className="fa fa-film"></i> : {data.imdbRuntime}
+          </span>
+          <span>
+            Year <i className="fa fa-calendar"></i> : {data.imdbYear}
+          </span>
+        </div>
+        <div className="movie-plot">
+          {data.Plot}
+        </div>
+        <div className="movie-info">
+          <div>
+            <span>Director</span>
+            <span>{data.Director}</span>
+          </div>
+          <div>
+            <span>Stars</span>
+            <span>{data.Actors}</span>
+          </div>
+          <div>
+            <span>Genres</span>
+            <span>{data.Genres}</span>
+          </div>
+          <div>
+            <span>Languages</span>
+            <span>{data.Languages}</span>
+          </div>
+          <div>
+            <span>Awards</span>
+            <span>{data.Awards}</span>
+          </div>
+        </div>
+      </div>
+      <div className="section-right">
+        <img src={data.Poster} alt={data.Title} />
+      </div>
+    </div>
   );
 };
 
