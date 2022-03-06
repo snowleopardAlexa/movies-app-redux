7fc5a82791e876bc65ba46d590bf5cba64f24bbb
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-06 16:43:09 -0500

    create async creator

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,15 @@
 import { useEffect } from 'react';
 import MovieList from "../MovieList/MovieList";
-import movieApi from "../../common/api/movieApi";
-import { APIKey } from "../../common/api/MovieApiKey";
 import { useDispatch } from "react-redux";
 import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
-
-const movieText = "Harry";
+  
 const dispatch = useDispatch();
 
 useEffect(() => {
   const fetchMovies = async () => {
-     const response = await movieApi
-     .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-     .catch((err) => {
-      console.log("Err :", err);
-     });
-    
-     dispatch(addMovies(response.data))
+ 
   };
 
   fetchMovies();
--- src/features/movies/movieSlice.jsx
+++ src/features/movies/movieSlice.jsx
@@ -1,4 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import movieApi from "../../common/api/movieApi";
+import { APIKey } from "../../common/api/MovieApiKey";
+
+// api call with async creator
+const fetchAsyncMovies = createAsyncThunk('movies/fetchAsyncMovies', 
+    async() => {
+    const movieText = "Harry";
+       
+    const response = await movieApi
+    .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+   
+    return response.data;
+})
 
 const initialState = {
     movies: {},
@@ -11,7 +24,19 @@ const movieSlice = createSlice({
         addMovies: (state, {payload}) => {
             state.movies = payload;
         },
-    }
+    },
+    extraReducers: {
+        [fetchAsyncMovies.pending]: () => {
+            console.log("pENDING");
+        },
+        [fetchAsyncMovies.fulfilled]: (state, {payload}) => {
+            console.log("Fetched Successfully!");
+            return {...state, movies: payload };
+        },
+        [fetchAsyncMovies.rejected]: () => {
+            console.log("Rejected");
+        },
+    },
 });
 
 export const { addMovies } = movieSlice.actions;
