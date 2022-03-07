8a7c608cb15072fd830fb526975d567076a985b4
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-06 19:26:29 -0500

    add loading

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -28,6 +28,10 @@ function MovieDetail() {
 
   return (
     <div className="movie-section">
+      {Object.keys(data).length === 0 ? 
+      (<div>...Loading</div>)
+      : ( 
+      <>
       <div className="section-left">
         <div className="movie-title">{data.Title}</div>
         <div className="movie-rating">
@@ -73,6 +77,8 @@ function MovieDetail() {
       <div className="section-right">
         <img src={data.Poster} alt={data.Title} />
       </div>
+      </>
+      )}
     </div>
   );
 };
