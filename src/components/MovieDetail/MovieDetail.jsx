b99419d7063d8246a59c87e0e816a18678a7cdb1
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-06 19:09:50 -0500

    add colors and fix icons

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,4 +1,8 @@
 import { useEffect } from "react";
+import "./MovieDetail.scss";
+import { BsFillCalendarWeekFill, BsFillHandThumbsUpFill } from "react-icons/bs";
+import { AiFillStar } from "react-icons/ai"
+import { FiFilm } from "react-icons/fi";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAsyncMovieOrShowDetail, getSelectedMovieOrShow } from "../../features/movies/movieSlice";
@@ -19,16 +23,16 @@ function MovieDetail() {
         <div className="movie-title">{data.Title}</div>
         <div className="movie-rating">
           <span>
-            IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
+            IMDB Rating <AiFillStar /> : {data.imdbRating}
           </span>
           <span>
-            IMDB Votes <i className="fa fa-thumbs-up"></i> : {data.imdbVotes}
+            IMDB Votes <BsFillHandThumbsUpFill /> : {data.imdbVotes}
           </span>
           <span>
-            Runtime <i className="fa fa-film"></i> : {data.imdbRuntime}
+            Runtime <FiFilm /> : {data.Runtime}
           </span>
           <span>
-            Year <i className="fa fa-calendar"></i> : {data.imdbYear}
+            Year <BsFillCalendarWeekFill /> : {data.Year}
           </span>
         </div>
         <div className="movie-plot">
@@ -45,11 +49,11 @@ function MovieDetail() {
           </div>
           <div>
             <span>Genres</span>
-            <span>{data.Genres}</span>
+            <span>{data.Genre}</span>
           </div>
           <div>
             <span>Languages</span>
-            <span>{data.Languages}</span>
+            <span>{data.Language}</span>
           </div>
           <div>
             <span>Awards</span>
