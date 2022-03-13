e01cd980276d58944f7964c4e0807507dfe8f72a
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-12 20:23:16 -0500

    add function submit handler and input

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import { Link } from 'react-router-dom';
 import { FaUserCircle} from 'react-icons/fa';
 import "./Header.scss";
 
 function Header() {
+
+const [term, setTerm] = useState("");
+
+const submitHandler = (e) => {
+  e.preventDefault();
+  console.log(term);
+}
+
     return (
      <div className="header">
-       <Link to="/">
-         <div className="logo">Movie App</div>   
-       </Link>
+         <div className="logo">
+           <Link to="/">Movie App</Link>
+         </div>   
+       <div className="search-bar">
+         <form onSubmit={submitHandler}>
+           <input 
+             type="text" 
+             value={term} 
+             placeholder="Search Movies or Shows" 
+             onChange={(e) => setTerm(e.target.value)} />
+           <button type="submit"><i className="fa fa-search"></i></button>
+         </form>
+       </div>  
        <div className="user-image">
          <FaUserCircle />
        </div>
